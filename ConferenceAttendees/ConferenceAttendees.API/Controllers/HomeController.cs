f999f4c731247ec52089e7fd442069cbf705e0df
Saeed <green.kolah@gmail.com>
2026-07-02 10:01:04 +0330

    Added Entity  And Configuration

---
+++ ConferenceAttendees/ConferenceAttendees.API/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConferenceAttendees.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController(IConfiguration configuration) : ControllerBase
+    {
+        private readonly IConfiguration _configuration = configuration;
+
+        [HttpGet]
+        public async Task<string> Get()
+        {
+            return _configuration["EnvironmentValue"];
+        }
+    }
+}
